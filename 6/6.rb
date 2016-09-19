a7e05d9453d43298e737847ebf23d154c394d232
Gavin Anderegg <gavin@anderegg.ca>
2016-09-18 21:28:45 -0300

    Day 6 (Much later!!!)

--- 6/6.rb
+++ 6/6.rb
@@ -1,9 +1,48 @@
 lights = []
+lightsCount = 0
 
 (0..999).each do |x|
-  lights[x] = 0
+  lightsCol = []
+
+  (0..999).each do |y|
+    lightsCol[y] = 0
+  end
+
+  lights[x] = lightsCol
 end
 
 File.readlines('input').each do |line|
+  m = line.match(/(\w+\s?\w[^\s\d,]+) (\d+),(\d+) through (\d+),(\d+)/)
 
+  command = m.captures[0]
+  startX = Integer(m.captures[1])
+  startY = Integer(m.captures[2])
+  endX = Integer(m.captures[3])
+  endY = Integer(m.captures[4])
+
+  (startY..endY).each do |row|
+    (startX..endX).each do |col|
+      if command == 'turn on'
+        lights[col][row] = 1
+      elsif command == 'turn off'
+        lights[col][row] = 0
+      else command == 'toggle'
+        if lights[col][row] == 1
+          lights[col][row] = 0
+        else
+          lights[col][row] = 1
+        end
+      end
+    end
+  end
 end
+
+(0..999).each do |x|
+  (0..999).each do |y|
+    if lights[x][y] == 1
+      lightsCount += 1
+    end
+  end
+end
+
+p lightsCount
