510eb2c51778967b835d1a84392a737c43e7a54f
Gavin Anderegg <gavin@anderegg.ca>
2016-12-04 18:16:25 -0400

    Getting rid of excess puts

--- 3/1.rb
+++ 3/1.rb
@@ -2,12 +2,10 @@ right = 0
 
 File.readlines('input').each do |line|
   m = line.match(/(\d+)\s+(\d+)\s+(\d+)/)
-  sides = m.captures.map {|x| x.to_i }
-  sides = sides.sort
+  sides = m.captures.map {|x| x.to_i }.sort
+
   if sides[0] + sides[1] > sides[2]
     right += 1
-  else
-    puts "Wrong: " + sides[0].to_s + " " + sides[1].to_s + " " + sides[2].to_s
   end
 end
 
