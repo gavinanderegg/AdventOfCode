81577b0cd1b0c8307074a9d8f86f5a4d145a666e
Gavin Anderegg <gavin@anderegg.ca>
2016-12-27 21:39:33 -0400

    non-working d7pt2... but this should be working

---
+++ 7/2.rb
@@ -0,0 +1,18 @@
+ssl = 0
+
+File.readlines('input').each do |line|
+  good = false
+
+  first = line.match(/[^\[]*(.)(.)\1[^\[]*\[.*[^\]]*(\2\1\2)/)
+  second = line.match(/\[[^\]]*(.)(.)\1[^\]]*\].*[\[]*(\2\1\2)/)
+
+  if (first) || (second)
+    good = true
+  end
+
+  if good
+    ssl += 1
+  end
+end
+
+puts ssl
