5965e21d24837750efae413deed3613fcdf3a747
Gavin Anderegg <gavin@anderegg.ca>
2016-12-28 14:43:28 -0400

    Gave up, and ended up solving it in TextMate 2's Find/Replace

--- 7/2.rb
+++ 7/2.rb
@@ -3,16 +3,27 @@ ssl = 0
 File.readlines('input').each do |line|
   good = false
 
-  first = line.match(/[^\[]*(.)(.)\1[^\[]*\[.*[^\]]*(\2\1\2)/)
-  second = line.match(/\[[^\]]*(.)(.)\1[^\]]*\].*[\[]*(\2\1\2)/)
+  # Ok, what am I trying to do here?
+  # I need to find out if there's a ABA outside of braces, and then I need to confirm that there's a BAB in braces. I should stop trying to do this all at the same time...
 
-  if first != nil
+  # ^aaaabaa[ or ]aabaaaa[ or ]aaabaaaa$
+  #  AND
+  # [aaaababaa]
+
+  first = line.match(/\][a-z]*([a-z])([a-z])\1.*?\[[^\]]*(\2\1\2)/)
+  second = line.match(/\[[a-z]*([a-z])([a-z])\1[^\]]*\].*?[\[]*(\2\1\2)/)
+
+  if (first != nil) && (first[1] != first[2])
     ssl += 1
-  end
-
-  if second != nil
+  elsif (second != nil) && (second[1] != second[2])
     ssl += 1
   end
 end
 
 puts ssl
+
+# 231
+
+# aaaabaa[aaaababaa]aaaaaaa[ or ]aaabaaaa$
+  #  AND
+  # ($|\])(?=[a-z]*)([a-z])([a-z])\1[a-z]*?($|\[)
