002827c04b292560dcd4fe3d209a761ca9cec186
Gavin Anderegg <gavinanderegg@users.noreply.github.com>
2021-12-03 11:24:24 -0400

    Start on 3 before getting interrupted

---
+++ 2021/3/3-1.cr
@@ -0,0 +1,14 @@
+input = File.read("test.txt")
+
+gamma = 0
+epsilon = 0
+
+input.each_line do |data|
+  bits = data.split("")
+
+
+end
+
+puts "Forward: " + forward.to_s
+puts "Height: " + depth.to_s
+puts "Mult: " + (forward * depth).to_s
