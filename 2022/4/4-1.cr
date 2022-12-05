47c0c89f6def602d9716a3ebc34d61fe29c252a5
Gavin Anderegg <gavinanderegg@users.noreply.github.com>
2022-12-05 09:31:45 -0400

    Day 4 pt 1

---
+++ 2022/4/4-1.cr
@@ -0,0 +1,20 @@
+input = File.read("input.txt")
+
+subsets = 0
+
+input.each_line do |line|
+  ranges = line.split(",")
+  range1 = ranges[0].split("-")
+  range2 = ranges[1].split("-")
+  range1_array = (range1[0].to_i..range1[1].to_i).to_a
+  range2_array = (range2[0].to_i..range2[1].to_i).to_a
+
+  if (
+    ((range1_array & range2_array) <=> range1_array) == 0 ||
+    ((range1_array & range2_array) <=> range2_array) == 0
+  )
+    subsets += 1
+  end
+end
+
+puts subsets
