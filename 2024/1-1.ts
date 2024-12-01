401310fcc918f421287e0019770b61874490c5ec
Gavin Anderegg <gavinanderegg@users.noreply.github.com>
2024-12-01 17:06:00 -0400

    Starting 2024

---
+++ 2024/1-1.ts
@@ -0,0 +1,25 @@
+const data = await Deno.readTextFile('1-1.txt')
+const lines = data.split('\n')
+
+let left = []
+let right = []
+
+let distanceTotal = 0;
+
+lines.forEach((item) => {
+	let numbers = item.split(/\s+/)
+
+	left.push(numbers[0])
+	right.push(numbers[1])
+})
+
+left.sort()
+right.sort()
+
+left.forEach((item, index) => {
+	var distance = Math.abs(item - right[index])
+
+	distanceTotal += distance
+})
+
+console.log(distanceTotal)
