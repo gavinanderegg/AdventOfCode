ee5251e17508eb592bfd2682de44c5c9e09071ea
Gavin Anderegg <gavinanderegg@users.noreply.github.com>
2024-12-02 21:34:16 -0400

    This is a hacky mess!

---
+++ 2024/2-1.ts
@@ -0,0 +1,59 @@
+const data = await Deno.readTextFile('2.txt')
+const lines = data.split('\n')
+
+var safeCount = 0
+
+lines.forEach((item) => {
+	let nums = item.split(' ').map((num) => Number(num))
+	let lastNum = null
+	var isSafe = true
+	var direction = 0
+
+	nums.forEach((num, iter) => {
+		var newDirection = 0
+
+		if (isSafe === true) {
+			if (lastNum === null) {
+				lastNum = num
+				return
+
+			} else {
+				if (direction === 0) {
+					direction = num - lastNum
+					direction = direction / Math.abs(direction)
+
+					if (Math.abs(num - lastNum) > 3 || Math.abs(num - lastNum) < 1) {
+						isSafe = false
+						return
+					}
+
+					lastNum = num
+
+					return
+
+				} else {
+					newDirection = num - lastNum
+					newDirection = newDirection / Math.abs(newDirection)
+
+					if (newDirection !== direction) {
+						isSafe = false
+						return
+					}
+
+					if (Math.abs(num - lastNum) > 3 || Math.abs(num - lastNum) < 1) {
+						isSafe = false
+						return
+					}
+
+					lastNum = num
+
+					if (iter === (nums.length - 1)) {
+						safeCount += 1
+					}
+				}
+			}
+		}
+	})
+})
+
+console.log(safeCount)
